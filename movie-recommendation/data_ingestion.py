0c8f5586d3237aab2e8a60e9ad6f6d5108dc2741
M. Arkan Nibrastama <59256068+ArkanNibrastama@users.noreply.github.com>
2023-03-24 17:48:01 +0700

    Add files via upload

---
+++ movie-recommendation/data_ingestion.py
@@ -0,0 +1,38 @@
+from google.cloud import storage
+import os
+import pandas as pd
+import json
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./key.json"
+
+
+gcs = storage.Client()
+bucket = gcs.bucket(bucket_name='{YOUR BUCKET NAME}')
+
+df = pd.read_csv('./dataset/imdb_top_1000.csv')
+
+# field name
+key = df.columns.values
+
+
+for i in range(len(df)):
+
+    # record value of i-th row
+    values = df.loc[i].values
+
+    movie = dict()
+
+    movie['id'] = i
+    for k, v in zip(key, values):
+
+        movie[k] = str(v)
+
+    print("="*100)
+    print(json.dumps(movie))
+
+    num = "%04d" % i
+    blob = bucket.blob(f'movie-{num}')
+    blob.upload_from_string(json.dumps(movie), 'application/json')
+
+    print("Upload Success!")
+    print("="*100)
